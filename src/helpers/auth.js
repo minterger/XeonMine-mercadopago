c726c7f47aac3a7c4349919c91d67041648e06e2
minterger <minterger@gmail.com>
2021-04-07 00:37:16 -0300

    update

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -3,19 +3,21 @@ const helpers = {};
 helpers.isAuthenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
+    } else {
+        res.status(404);
+        req.flash('error_msg', 'Esta pagina no existe, fuiste redireccionado');
+        res.redirect('/');
     }
-    res.status(404);
-    req.flash('error_msg', 'Esta pagina no existe, fuiste redireccionado');
-    res.redirect('/');
 }
 
 helpers.isAuthenticatedFalse = (req, res, next) => {
     if (!req.isAuthenticated()) {
         return next();
+    } else {
+        res.status(404);
+        req.flash('error_msg', 'No Puedes Registrarte o Iniciar Sesion con una Sesion ya Iniciada');
+        res.redirect('/');
     }
-    res.status(404);
-    req.flash('error_msg', 'No Puedes Registrarte o Iniciar Sesion con una Sesion ya Iniciada');
-    res.redirect('/');
 }
 
 module.exports = helpers;
