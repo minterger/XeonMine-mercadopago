e74bb710dda0320d508a8c49829bbc861719c269
minterger <minterger@gmail.com>
2021-04-08 02:31:57 -0300

    fix

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,9 +1,5 @@
 const discord = document.getElementById('discord')
 
 discord.addEventListener('click', () => {
-    discord.classList.add('discord_active')
-    discord.classList.replace('discordHover', 'discord_active')
-    setTimeout(() => {
-        window.open('https://discord.gg/A6SS8AbSGD', '_self')
-    }, 500)
+    window.open('https://discord.gg/A6SS8AbSGD', '_self')
 })
